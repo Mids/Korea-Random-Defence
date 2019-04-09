f2434e4c56a399eb779a64111d1e0bdab9ebb5c9
Jin Park <jiny199109@gmail.com>
2019-04-10 07:24:11 +0900

    Selection circle

---
+++ Assets/02. Scripts/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	private RTSManager _rtsManager;
+	private Projector _selectionCircle;
+	public bool IsSelected;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_rtsManager = GameObject.FindGameObjectWithTag("RTSManager").GetComponent<RTSManager>();
+		_selectionCircle = GetComponentInChildren<Projector>();
+		Deselect();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_rtsManager.IsWithinSelectionBounds(gameObject))
+		{
+			Select();
+		}
+		else
+		{
+			Deselect();
+		}
+	}
+
+	public void Select()
+	{
+		IsSelected = true;
+		_selectionCircle.enabled = true;
+	}
+
+	public void Deselect()
+	{
+		IsSelected = false;
+		_selectionCircle.enabled = false;
+	}
+}
