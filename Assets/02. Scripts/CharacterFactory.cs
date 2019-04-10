1e5397230546eb2efe097e08de36cd4bffa9c8c7
Jin Park <jiny199109@gmail.com>
2019-04-10 10:21:10 +0900

    Move characters on right click

--- Assets/02. Scripts/CharacterFactory.cs
+++ Assets/02. Scripts/CharacterFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace KRD
 {
@@ -9,6 +10,7 @@ namespace KRD
 		public Character[] SpawnableCharacters;
 		private RTSManager _rtsManager;
 		public List<Character> CurrentCharacters = new List<Character>();
+		public List<Character> SelectedCharacters = new List<Character>();
 
 		// Start is called before the first frame update
 		void Start()
@@ -20,22 +22,57 @@ namespace KRD
 		// Update is called once per frame
 		void Update()
 		{
+			// Select units if player is selecting
 			if (_rtsManager.IsSelecting)
 			{
 				CheckSelection();
+				return;
+			}
+
+			// If we press the right mouse button, let the characters go
+			if (Input.GetMouseButton(1))
+			{
+				MoveFormation();
 			}
 		}
 
+		/// <summary>
+		/// Move selected characters as a formation
+		/// </summary>
+		void MoveFormation()
+		{
+			foreach (var character in SelectedCharacters)
+			{
+				var nav = character.GetComponent<NavMeshAgent>();
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				if (Physics.Raycast(ray, out var hit))
+				{
+					// TODO: Keep the formation
+					nav.destination = hit.point;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Check if selected
+		/// </summary>
 		void CheckSelection()
 		{
 			foreach (var character in CurrentCharacters)
 			{
 				if (_rtsManager.IsWithinSelectionBounds(character.gameObject))
 				{
+					if (character.IsSelected) continue;
+
+					SelectedCharacters.Add(character);
 					character.Select();
 				}
 				else
 				{
+					if (!character.IsSelected) continue;
+
+					SelectedCharacters.Remove(character);
 					character.Deselect();
 				}
 			}
