d37b227a87fe289a47784473c468ee4df85b710f
gangrae <gang31115@gmail.com>
2019-09-10 03:50:29 +0900

    Uncommon Character

---
+++ Assets/02. Scripts/Characters/Uncommon/Robin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KRD
+{
+	public class Robin : Character
+	{
+		// Start is called before the first frame update
+		protected override void Start()
+		{
+			base.Start();
+			AttackDamage = 58;
+			StunAbility = false;
+		}
+
+
+		// Update is called once per frame
+		protected override void Update()
+		{
+			base.Update();
+		}
+	}
+}
