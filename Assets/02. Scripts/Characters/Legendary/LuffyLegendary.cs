229b58dfc1447a347865f1d3e8e5d54ef48daf85
gangrae <gang31115@gmail.com>
2019-09-11 14:29:49 +0900

    add Legendary character

---
+++ Assets/02. Scripts/Characters/Legendary/LuffyLegendary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KRD
+{
+	public class LuffyLegendary : Character
+	{
+		// Start is called before the first frame update
+		protected override void Start()
+		{
+			base.Start();
+			AttackDamage = 20751;
+			StunAbility = false;
+		}
+
+
+		// Update is called once per frame
+		protected override void Update()
+		{
+			base.Update();
+		}
+	}
+}
